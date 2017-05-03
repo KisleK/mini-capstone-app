5cbaa6cfc08890441654db5bbc480407aa1cfb92
KisleK <kisle.kuhn1@gmail.com>
2017-05-02 21:30:29 -0500

    Added first product instance

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+product = Product.new(name: "MacBook Pro", price: 1100, image: "http://kids.nationalgeographic.com/content/dam/kids/photos/animals/Birds/H-P/mallard-male-standing.jpg.adapt.945.1.jpg", description: "This is an awesome MacBook Pro")
